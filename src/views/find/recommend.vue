ec6f862fead292c850a7f462d189a64de329b2b4
qinmingjie <1934394876@qq.com>
2023-08-03 12:17:44 +0800

    fix(recommend): 更改热门推荐展示

--- src/views/find/recommend.vue
+++ src/views/find/recommend.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="recommend-container" v-loading="loading">
     <TitleLink :links="links" :options="options" />
-    <el-row v-for="(chunk, chunkIndex) in recommendList" :key="chunkIndex">
-      <el-col v-for="(recommend, index) in chunk" :key="recommend.id" :span="4" :offset="index > 0 ? 1 : 0">
+    <el-row v-for="(chunk, chunkIndex) in handlerList" :key="chunkIndex">
+      <el-col v-for="(recommend, index) in chunk" :key="recommend.id" :span="span" :offset="index > 0 ? offset : 0">
         <PreviewItem :info="recommend" :path="jumpPath(recommend.id)" />
       </el-col>
     </el-row>
@@ -33,6 +33,8 @@ export default {
         isShowIcon: true,
         hidCursor: true
       },
+      span: 4,
+      offset: 1,
       links: [{ name: "推荐歌单", path: false }]
     };
   },
@@ -44,6 +46,13 @@ export default {
       return (id) => {
         return `/playlist-detail/${id}`;
       };
+    },
+    clientWidth() {
+      return this.$clientWidth.value;
+    },
+
+    handlerList() {
+      return this.clientWidth <= 500 ? chunkArray(this.recommendList, 2) : chunkArray(this.recommendList, 5);
     }
   },
   methods: {
@@ -53,11 +62,11 @@ export default {
         if (this.$isLogin()) {
           // 获取登陆后的热门推荐
           const res = await getLoginRecommendPlaylist({ limit: 20 });
-          this.recommendList = chunkArray((res.data?.recommend || []).slice(0, 20), 5);
+          this.recommendList = res.data?.recommend || []; // chunkArray((res.data?.recommend || []).slice(0, 20), 5);
         } else {
           // 获取普通的热门推荐
           const res = await getRecommendPlaylist({ limit: 20 });
-          this.recommendList = chunkArray((res.data?.result || []).slice(0, 20), 5);
+          this.recommendList = res.data?.result || []; //chunkArray((res.data?.result || []).slice(0, 20), 5);
         }
       } catch (error) {
         console.error(error);
@@ -72,6 +81,17 @@ export default {
     info: {
       handler() {
         this.getRecommendList();
+      },
+      immediate: true
+    },
+    clientWidth: {
+      handler(val) {
+        if (val <= 500) {
+          (this.span = 11), (this.offset = 2);
+          return;
+        }
+        this.span = 4;
+        this.offset = 1;
       },
       immediate: true
     }
