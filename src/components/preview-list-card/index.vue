2845841d4b772e71b7b4476e80951042d7bf32d4
qinmingjie <1934394876@qq.com>
2023-08-08 16:13:07 +0800

    fix(previewListCard): 更改卡片列表组件为通用组件替代单独的previewCard

--- src/components/preview-list-card/index.vue
+++ src/components/preview-list-card/index.vue
@@ -1,37 +1,65 @@
 <template>
   <!-- 响应式卡片列表 -->
   <div class="preview-list-card-comp">
-    <div class="preview-list-card-wrap">
-      <PreviewCard
-        v-for="list in lists"
-        :key="list.id"
-        :info="list"
-        :path="baseSkipPath + list.id"
-        :class="cardColumnClass"
-      />
+    <div class="preview-list-card-wrap" v-if="lists.length">
+      <!-- 音乐卡片 -->
+      <div :class="['preview-item', isResponse ? cardColumnClass : '']" v-for="list in lists" :key="list.id">
+        <div class="preview-img">
+          <el-image :src="list.picUrl + '?param=400y400'" loading="lazy">
+            <template #placeholder>
+              <div class="image-place-slot"><img src="../../assets/img-place.png" /></div>
+            </template>
+            <template #error>
+              <div class="image-error-slot"><img src="../../assets/img-failed.png" /></div>
+            </template>
+          </el-image>
+          <span class="play" v-if="isShow('playIcon')">
+            <el-icon :style="{ left: iconPosition[0], top: iconPosition[1] }"><IEpCaretRight /></el-icon>
+          </span>
+          <span class="play-count" v-if="isShow('playCount')">{{ playCount(list.playcount || list.playCount) }}</span>
+          <span class="mask" v-if="isShow('mask')"></span>
+          <span class="to-jump" @click="jump(list)"></span>
+        </div>
+        <p class="name" v-if="isShow('name')" @click="jump(list)">{{ list.name }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from "vue";
-const PreviewCard = defineAsyncComponent(() => import("@/components/preview-card/index.vue"));
+import { formatPlayCount } from "@/utils/tool";
 export default {
   name: "PreviewListCard",
   props: {
+    // 卡片数据
     lists: {
       type: Array
     },
-    baseSkipPath: {
-      type: String,
-      default: "/playlist-detail/"
+    // 卡片展示的内容
+    show: {
+      type: Array,
+      default() {
+        return ["name", "playIcon", "playCount", "mask"];
+      }
     },
-    disabled: {
+    // 播放按钮的定位
+    iconPosition: {
+      type: Array,
+      default() {
+        return ["76%", "76%"];
+      }
+    },
+    // 点击跳转的路径
+    path: {
+      type: String
+    },
+    // 卡片是否响应式布局
+    isResponse: {
       type: Boolean,
-      default: true
+      default: false
     }
   },
-  components: { PreviewCard },
+  components: {},
   data() {
     return {
       columnName: "column5"
@@ -48,9 +76,21 @@ export default {
         column5: "column5"
       };
       return classMap[this.columnName];
+    },
+    isShow() {
+      return (type) => {
+        return this.show.some((item) => item === type);
+      };
     }
   },
-  methods: {},
+  methods: {
+    jump(params) {
+      this.$emit("cardClick", params);
+    },
+    playCount(count) {
+      return formatPlayCount(count);
+    }
+  },
   created() {},
   mounted() {},
   watch: {
@@ -94,6 +134,102 @@ export default {
     margin-right: 2.5%;
     &:nth-of-type(5n) {
       margin-right: 0;
+    }
+  }
+}
+
+.preview-item {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  .preview-img {
+    width: 100%;
+    border-radius: 10px;
+    position: relative;
+    margin-bottom: 10px;
+    cursor: pointer;
+    .play-count {
+      position: absolute;
+      z-index: 3;
+      color: rgba(255, 255, 255, 0.9);
+      right: 10px;
+      top: 4px;
+      font-size: 0.8em;
+    }
+    .to-jump {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 2;
+    }
+    .mask {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1;
+      border-radius: 10px;
+      background-color: rgba(0, 0, 0, 0.2);
+      opacity: 0;
+      transition: all 0.6s;
+    }
+    &:hover {
+      .mask,
+      .play .el-icon {
+        opacity: 1;
+      }
+    }
+  }
+  .el-image {
+    border-radius: 10px;
+    box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.2);
+    display: block;
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+    :deep(img) {
+      display: block;
+      width: 100%;
+      height: auto;
+    }
+  }
+  .play {
+    .el-icon {
+      width: 13%;
+      height: 13%;
+      display: flex;
+      justify-content: center;
+      align-self: center;
+      z-index: 3;
+      border-radius: 50%;
+      background-color: rgba(255, 255, 255, 0.9);
+      position: absolute;
+      color: $color-primary;
+      text-align: center;
+      transition: all 0.6s;
+      opacity: 0;
+      svg {
+        font-size: 1.2em;
+      }
+    }
+  }
+  .name {
+    margin-top: 0;
+    font-size: 0.9em;
+    opacity: 0.9;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-bottom: 1.2em;
+    cursor: pointer;
+    &:hover {
+      opacity: 1;
     }
   }
 }
